a21589cfb9231b04ebe8f5a156650f44126caeea
Ruan Araujo <ruanaraujo8@gmail.com>
2022-02-05 17:09:13 -0300

    buscando por id

---
+++ src/main/java/com/ruandev/querolanche/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,24 @@
+package com.ruandev.querolanche.jpa;
+
+import com.ruandev.querolanche.QuerolancheApiApplication;
+import com.ruandev.querolanche.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class BuscaCozinhaMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext =
+                new SpringApplicationBuilder(QuerolancheApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+        System.out.println(cozinha.getNome());
+    }
+
+}
