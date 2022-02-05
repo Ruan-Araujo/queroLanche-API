9dce8db4dfdc5e0ae0f7b4685b0b86cb00a03e35
Ruan Araujo <ruanaraujo8@gmail.com>
2022-02-05 17:19:47 -0300

    atualizando objeto no banco de dados

--- src/main/java/com/ruandev/querolanche/jpa/AlterecaoCozinhaMain.java
+++ src/main/java/com/ruandev/querolanche/jpa/AlterecaoCozinhaMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AlterecaoCozinhaMain {
 
     public static void main(String[] args) {
 
@@ -17,17 +17,11 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1  = new Cozinha();
-        cozinha1.setNome("Italiana");
+        Cozinha cozinha  = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Italiana");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
-
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+        cadastroCozinha.salvar(cozinha);
     }
 
 }
