f115fac48d2b44c8515ca685d46419a5da4ca70d
Ruan Araujo <ruanaraujo8@gmail.com>
2022-02-10 20:33:35 -0300

    implementando padrão repository

---
+++ src/main/java/com/ruandev/querolanche/domain/repository/CozinhaRepository.java
@@ -0,0 +1,14 @@
+package com.ruandev.querolanche.domain.repository;
+
+import com.ruandev.querolanche.domain.model.Cozinha;
+
+import java.util.List;
+
+public interface CozinhaRepository {
+
+    List<Cozinha> listar();
+    Cozinha buscar(Long id);
+    Cozinha salvar(Cozinha cozinha);
+    void remover(Cozinha cozinha);
+
+}
